1a13bb0e1df42bcae6203916d7e4626f77122c2d
vladstasyuk <vladislav.stasiuk@stud.onu.edu.ua>
2020-10-06 14:33:06 +0300

    #51 solution

--- ONU_python/Lab2/variant_3/labwork_3_3.py
+++ ONU_python/Lab2/variant_3/labwork_3_3.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def Silverster(a):
+def silverster(a):
     n = len(a)
     check = True
     for i in range(n):
@@ -9,9 +9,3 @@ def Silverster(a):
             check = False
             break
     return check
-
-arr1 = np.array([[-2, 8, 8, 4, -3], [3, 0, -1, 3, 0], [7, -2, -4, 6, 2], [7, -5, 1, -5, -3], [-2, -5, 7, 6, -1]])
-arr2 = np.array([[14, 6, 3], [6, 9, -4], [3, -4, 9]])
-
-print(Silverster(arr1))
-print(Silverster(arr2))
