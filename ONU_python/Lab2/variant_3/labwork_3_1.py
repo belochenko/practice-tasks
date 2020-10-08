ee774daea530985e99a434a6e1d85ea675b8bc1a
vladstasyuk <vladislav.stasiuk@stud.onu.edu.ua>
2020-10-08 15:42:46 +0300

    #49 solution

---
+++ ONU_python/Lab2/variant_3/labwork_3_1.py
@@ -0,0 +1,20 @@
+import numpy as np
+import random
+
+def  v2normalize(x):
+    vector = np.linalg.norm(x)
+    if vector == 0:
+        return x
+    else:
+        return x/vector
+
+array1 = np.random.randint(-10, 10, size=(10, 2))
+distance = np.zeros(10)
+distance = np.linalg.norm(array1, axis=1)
+array2 = distance.argsort()
+norm_array = np.zeros((10, 2))
+norm_array = v2normalize(array1)
+print(array1[distance.argmax()], " - наиболее удаленная от начала координат точка")
+print(array1[array2], " - отсортированные в порядке возрастания длин векторов точки")
+print(norm_array, " - массив нормированных векторов")
+print(norm_array[norm_array > 0], " - массив вектор с положительными координатами")
