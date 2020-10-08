6a5b90ca302bb25c73206b28d1b071b518c9e6e3
HolyRave <ilya.balitskiy@stud.onu.edu.ua>
2020-10-09 00:32:26 +0300

    Reformated labwork_3_2

--- ONU_python/Lab2/variant_3/labwork_3_2.py
+++ ONU_python/Lab2/variant_3/labwork_3_2.py
@@ -1,10 +1,10 @@
 import numpy as np
 import math
 
-ar1 = np.array([[6,5,4,3,2,1]])
+ar1 = np.array([[6, 5, 4, 3, 2, 1]])
 ar2 = ar1 * -1
-ar3 = np.tile(np.array([[2,2],[2,2],[4,4],[4,4]]),3)
-block_array = np.concatenate([ar1,ar2,[ar3[0]],[ar3[1]],[ar3[2]],[ar3[3]]],axis=0)
+ar3 = np.tile(np.array([[2, 2], [2, 2], [4, 4], [4, 4]]), 3)
+block_array = np.concatenate([ar1, ar2, [ar3[0]], [ar3[1]], [ar3[2]], [ar3[3]]], axis=0)
 
 oldelem = math.inf
 for i in range(6):
