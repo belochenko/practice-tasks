22f8f1f44cfee132f7ac47a741cf5e670144ec4c
vladstasyuk <vladislav.stasiuk@stud.onu.edu.ua>
2020-10-13 17:38:05 +0300

    fix lab_work

--- ONU_python/Lab2/variant_3/labwork_3_5.py
+++ ONU_python/Lab2/variant_3/labwork_3_5.py
@@ -1,20 +1,8 @@
 import numpy as np
-import csv
-import re
 
+def function():
+    matrix = np.genfromtxt('input.csv', dtype=int, delimiter=',')
+    matrix[0][0] = np.sum(matrix)
+    np.savetxt('output.csv', matrix, fmt='%.0f')
 
-def function(f):
-    with open(f, 'r') as data:
-        reader = csv.reader(data, delimiter=",")
-        reader = list(reader)
-        reader = [[int(x) for x in line] for line in reader]
-        reader[0][0] = np.sum(reader)
-        reader = [[int(y) for y in x] for x in reader]
-        formate = re.sub('], +', '\n', str(reader))
-        formate = re.sub('[\[\]]+', '', formate)
-        with open("output.csv", "w") as f:
-            f.write(formate)
-
-
-file = "input.csv"
-function(file)
+function()
