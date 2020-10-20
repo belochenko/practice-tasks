5de4f4e6d6235559989674e8752078446b085774
HolyRave <ilya.balitskiy@stud.onu.edu.ua>
2020-10-20 21:22:39 +0300

    fixed task_6

--- ONU_python/Lab3/variant_3/task_6.py
+++ ONU_python/Lab3/variant_3/task_6.py
@@ -6,7 +6,7 @@ fig = plt.figure()
 x = np.linspace(-3, 3, 100)
 y = np.linspace(-3, 3, 100)
 (x1, y1) = np.meshgrid(x, y)
-f = (x1**2 + y1**2) - (7 * (x1**2 - y1**2))
+f = (x1**2 + y1**2)**2 - (7 * (x1**2 - y1**2))
 
 plt.contour(x1, y1, f, 0)
 plt.show()
