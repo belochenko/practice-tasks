a21a7cdffd49a02483da4dcc86325ce6a0128240
vladstasyuk <vladislav.stasiuk@stud.onu.edu.ua>
2020-11-18 17:47:06 +0200

    fix task_8

--- ONU_python/Lab5/task_8.py
+++ ONU_python/Lab5/task_8.py
@@ -2,5 +2,5 @@ import sympy as sp
 
 x = sp.symbols('x')
 y = sp.Function('y')
-sp.pprint(sp.dsolve(y(x).diff(x, 2) + 3*y(x).diff(x) + y(x) - sp.cos(x), y(x)))
+sp.pprint(sp.dsolve(y(x).diff(x, 2) - 3*y(x).diff(x) + y(x) - sp.cos(x), y(x)))
 
