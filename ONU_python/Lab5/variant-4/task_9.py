92e41ece87e9ad37a2c06deb045f56496b328158
Alexandr_Rozin <mega.time182@gmail.com>
2020-11-18 19:36:44 +0200

    fixed #169

--- ONU_python/Lab5/variant-4/task_9.py
+++ ONU_python/Lab5/variant-4/task_9.py
@@ -1,6 +1,6 @@
 import sympy as sp
 
 a, x, t = sp.symbols('a x t')
-u = (-1) / (2 * a) * sp.exp(sp.sqrt((x - t) ** 2) ** -a)
-res = sp.simplify(u.diff(x, 2) - a ** 2 * u)
-print(res)
+u = (-1) / (2 * a) * sp.exp(-a * sp.sqrt((x - t) ** 2))
+res = sp.simplify(u.diff(x, 2) - (a ** 2 * u))
+print(res == 0)
